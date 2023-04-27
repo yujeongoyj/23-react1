b1f365108e90fe33b1d80b7816ec27fb7d6de54a
yujeongoyj <oyj3989288@gmail.com>
2023-04-27 12:41:40 +0900

    change index.js

--- src/index.js
+++ src/index.js
@@ -3,23 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Clock from './chapter04/clock';
+import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 
-
-import Library from './chapter03.Library';
-import Clock from './chapter04/CommentList';
-
-
-setInterval(()=> {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Clock />
+    <ConfirmButton/>
   </React.StrictMode>
 );
-
-},1000);
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
